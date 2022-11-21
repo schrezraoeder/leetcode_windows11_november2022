7deaa6678f7c17f19c517e376a66c002e92ad309
Ezra Schroeder <ezra.schroeder@gmail.com>
2022-11-21 10:18:15 -0600

    Time: 192 ms (48.69%), Space: 15 MB (77.90%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.py
+++ 0402-remove-k-digits/0402-remove-k-digits.py
@@ -1,18 +1,23 @@
-# Note: i watched the neetcode solution 
-class Solution:
-    def removeKdigits(self, num: str, k: int) -> str:
+# trying to code it up again from memory after a little time 
+# passes... originally i watched the neetcode solution
+# and coded it up very shortly afterwards on the python3 tab 
+class Solution(object):
+    def removeKdigits(self, num, k):
+        """
+        :type num: str
+        :type k: int
+        :rtype: str
+        """
         stack = [] 
         for char in num:
-            while k > 0 and stack and stack[-1] > char: 
+            while stack and k > 0 and stack[-1] > char: 
+                stack.pop()
                 k -= 1 
-                stack.pop() 
-            stack.append(char) 
-        stack = stack[:len(stack)-k] 
-        res = "".join(stack) 
-        if res: 
-            res = str(int(res)) 
-        else:
-            res = "0"
-        return res 
-                
+            if stack or char != '0':
+                stack.append(char) 
+        stack = ''.join(stack) 
+        stack = stack[:len(stack)-k]
+        if not stack: stack = '0' 
+        return stack 
+        
         

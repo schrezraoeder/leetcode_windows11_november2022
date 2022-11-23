38af8b759aa15fc5095f5bb80e29b4baf2ed22fe
Ezra Schroeder <ezra.schroeder@gmail.com>
2022-11-22 21:33:25 -0600

    Time: 207 ms (19.32%), Space: 17.5 MB (25.53%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,4 +1,9 @@
-# NOTE: this is the neetcode solution on youtube at https://youtu.be/A8NUOmlwOlM?t=696  
+# NOTE: while I had watched the neetcode solution on youtube at https://youtu.be/A8NUOmlwOlM?t=696  
+# earlier today as I knew that 1. I would be peer mock interviewing with this question tonight & 2. 
+# that I was **way** too low on time to try to figure it out on my own today; 
+# I did not actually look at any outside resources nor google anything while coding it up from memory 
+# a number of hours later on in the evening & I would have to ask for critical feedback but my
+# perception is that I explained my thinking process pretty well to somebody who mostly codes Java & no python 
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         

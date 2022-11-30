609ab6942adf856d1b4bcb4b0557601641333f54
Ezra Schroeder <ezra.schroeder@gmail.com>
2022-11-30 10:17:49 -0600

    Time: 27 ms (98.78%), Space: 14 MB (64.81%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,23 +1,23 @@
-# googled for a problem like this on leetcode after spending a few seconds 
-# on https://www.udemy.com/course/python-for-data-structures-algorithms-and-interviews/learn/lecture/4309898#overview 
-
-from collections import Counter 
-
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
         
-        s = ''.join(s.split()) # replace s with a version of itself lacking spaces 
-        t = ''.join(t.split()) 
-        if len(s) != len(t):
-            return False 
-        q = Counter(s) 
-        w = Counter(t) 
-        for item in q:
-            if q[item] != w[item]:
-                return False 
-        for item in w:
-            if w[item] != q[item]:
-                return False 
-        return True 
+        freq_s = {}
+        freq_t = {} 
         
+        for char in s:
+            if char not in freq_s: 
+                freq_s[char] = 1
+            else:
+                freq_s[char] += 1
+        for char in t:
+            if char not in freq_t:
+                freq_t[char] = 1
+            else:
+                freq_t[char] += 1 
+        return freq_s == freq_t 
         

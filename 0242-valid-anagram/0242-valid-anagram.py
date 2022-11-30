8b803a89d31331e6d3149ad284863692de587b24
Ezra Schroeder <ezra.schroeder@gmail.com>
2022-11-30 10:24:47 -0600

    Time: 99 ms (46.17%), Space: 13.7 MB (90.38%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -8,36 +8,37 @@ class Solution(object):
         :rtype: bool
         """
         
-#         freq_s = {}
-#         freq_t = {} 
+        freq_s = {}
+        freq_t = {} 
         
-#         for char in s:
-#             if char not in freq_s: 
-#                 freq_s[char] = 1
-#             else:
-#                 freq_s[char] += 1
-#         for char in t:
-#             if char not in freq_t:
-#                 freq_t[char] = 1
-#             else:
-#                 freq_t[char] += 1 
-#         return freq_s == freq_t 
+        for char in s:
+            if char not in freq_s: 
+                freq_s[char] = 1
+            else:
+                freq_s[char] += 1
+        for char in t:
+            if char not in freq_t:
+                freq_t[char] = 1
+            else:
+                freq_t[char] += 1 
+        return freq_s == freq_t 
+    
         # alternative one-line solution: 
         # return ''.join(sorted(s)) == ''.join(sorted(t)) 
         
         # alternative solution using Counter's: 
-        s = ''.join(s.split()) # replace s with a version of itself lacking spaces 
-        t = ''.join(t.split()) 
-        if len(s) != len(t):
-            return False 
-        q = Counter(s) 
-        w = Counter(t) 
-        for item in q:
-            if q[item] != w[item]:
-                return False 
-        for item in w:
-            if w[item] != q[item]:
-                return False 
-        return True 
+        # s = ''.join(s.split()) # replace s with a version of itself lacking spaces 
+        # t = ''.join(t.split()) 
+        # if len(s) != len(t):
+        #     return False 
+        # q = Counter(s) 
+        # w = Counter(t) 
+        # for item in q:
+        #     if q[item] != w[item]:
+        #         return False 
+        # for item in w:
+        #     if w[item] != q[item]:
+        #         return False 
+        # return True 
         
         

05532f34399042ef0fbae0bfc693897d3dbc1fa0
Ezra Schroeder <ezra.schroeder@gmail.com>
2022-12-31 16:27:35 -0600

    Time: 59 ms (63.98%), Space: 16.7 MB (27.31%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -32,6 +32,13 @@ class Solution:
         if not root.right:
             if  bstMax(root.left) < root.val:
                 return self.isValidBST(root.left)   
+        # originally in the following line of code I did not have the `root.left and root.right` clause 
+        # immediately after the `if`... in other words the following line of code used to look like: 
+        # if bstMax(root.left) < root.val and bstMin(root.right) > root.val: 
+        # however the stupid thing failed... passing on 79 out of 82 test cases but failing for example on
+        # the test case:
+        # [-2147483648,null,2147483647,-2147483648] 
+        # but I feel that this is a **fluke** of leetcode but cannot be bothered presently to look into it 
         if root.left and root.right and bstMax(root.left) < root.val and bstMin(root.right) > root.val: 
             return self.isValidBST(root.left) and self.isValidBST(root.right) 
         return False 

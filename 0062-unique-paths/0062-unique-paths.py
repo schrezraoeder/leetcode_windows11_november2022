91ebbe1db7f1a38ac65f6ea920f9a00c92f037d1
Ezra Schroeder <ezra.schroeder@gmail.com>
2023-01-31 14:37:31 -0600

    Time: 32 ms (77.98%), Space: 13.9 MB (68.12%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -2,7 +2,7 @@ from itertools import product
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         dp = [[1 for _ in range(n)] for _ in range(m)] 
-        # the first row and first column will always remain 1 or these types of problems with no obstacles 
+        # the first row and first column will always remain 1 for these types of problems with no obstacles 
         for i, j in product (range(1, m), range(1, n)): 
             dp[i][j] = dp[i-1][j] + dp[i][j-1] 
         return dp[-1][-1]

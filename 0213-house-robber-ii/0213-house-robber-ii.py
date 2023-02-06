054c8fac15d9a12d311abbfb77c448680c0bc04e
Ezra Schroeder <ezra.schroeder@gmail.com>
2023-02-06 14:39:21 -0600

    Time: 23 ms (46.97%), Space: 13.5 MB (18.67%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        def house_robber_i(nums):
+            dp = [0] * len(nums)
+            dp[0] = nums[0]
+            if len(nums) <= 1:
+                return dp[0] 
+            dp[1] = nums[1] 
+            if len(nums) == 2:
+                return max(nums)
+            if len(nums) == 3:
+                return max(nums[0] + nums[2], nums[1]) 
+            dp[2] = max(nums[0] + nums[2], nums[1]) 
+            for x in range(3, len(nums)):
+                dp[x] = max(dp[x-2] + nums[x], dp[x-1], dp[x-3] + nums[x])  
+            print (dp) 
+            return dp[-1] 
+        if len(nums) == 1:
+            return nums[0] 
+        return max(house_robber_i(nums[:-1]), house_robber_i(nums[1:]))
+        
+        

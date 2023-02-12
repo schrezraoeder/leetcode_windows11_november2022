ad4c7b97743c299f183be234756fdcbb9872220a
Ezra Schroeder <ezra.schroeder@gmail.com>
2023-02-12 17:17:15 -0600

    Time: 122 ms (72.20%), Space: 13.8 MB (97.67%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,32 +1,17 @@
-# Note: I watched the neetcode solution video first 
-
-class Solution(object):
-    def count_from_position(self, s, l, r, count):
-        while l >= 0 and r < len(s) and s[l] == s[r]:
-            count += 1 
-            l -= 1 
-            r += 1 
-        return count 
-    
-    def countSubstrings(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        count = 0 # count of number of palindromic substrings found so far 
-        for i in range(len(s)): 
-            # count odd-lengthed palindromic substrings 
-            l = r = i 
-            count = self.count_from_position(s, l, r, count) 
-            # while l >= 0 and r < len(s) and s[l] == s[r]:
-            #     count += 1 
-            #     l -= 1 
-            #     r += 1 
-            l, r = i, i + 1 
-            count = self.count_from_position(s, l, r, count)
-            # while l >= 0 and r < len(s) and s[l] == s[r]: 
-            #     count += 1 
-            #     l -= 1 
-            #     r += 1 
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        count = 0 
+        for i in range(len(s)):
+            l, r = i, i 
+            # count += 1 
+            while l >= 0 and r < len(s) and s[l] == s[r]: 
+                count += 1 
+                l -= 1 
+                r += 1 
+            l, r = i, i+1 
+            while l >= 0 and r < len(s) and s[l] == s[r]: 
+                count += 1 
+                l -= 1 
+                r += 1 
         return count 
         

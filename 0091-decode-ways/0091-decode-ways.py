24c978429890c644cf846431ad683f0fd99e2c46
Ezra Schroeder <ezra.schroeder@gmail.com>
2023-02-14 10:43:07 -0600

    Time: 44 ms (21.98%), Space: 13.8 MB (73.29%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,21 +1,28 @@
-# probably identical to the Neetcode solution b/c I have *really* struggled with this one for 
-# unknown reasons
-# https://youtu.be/_i4Yxeh5ceQ?t=5763 
-class Solution(object):
-    def numDecodings(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        if s[0] == '0': return 0 
-        dp = {len(s):1} # ? 
-        for j in range(len(s)-1, -1, -1): 
-            if s[j] == '0': 
-                dp[j] = 0 
-            else:
-                dp[j] = dp[j+1] 
-            if (j+1 < len(s)) and ((s[j] == '1') or (s[j] == '2' and s[j+1] in '0123456')):
-                dp[j] += dp[j+2] 
-        return dp[0] 
+# NOTE: after much struggle with this one, watched the Neetcode solution before coding it up 
+# And I struggled so much with this one that I also watched the Knowledge Center solution[s] 
+# then tried coding it up again 
+class Solution: 
+    def numDecodings(self, s: str) -> int:  
+        if len(s) == 0: 
+            return 0 
+        if s[0] == '0':
+            return 0 
+        if len(s) == 1: 
+            return 1 
+        count1 = 1 # prev prev 
+        count2 = 1 # prev 
+        for i in range(1, len(s)):
+            count = 0 
+            if s[i] in '123456789':
+                count += count2 
+            if (s[i-1] == '1' or s[i-1] == '2' and s[i] in '0123456'):
+                count += count1 
+            count1 = count2 
+            count2 = count 
+        return count 
+        
             
+        
+                
+        
         

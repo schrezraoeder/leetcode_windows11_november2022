acc47b6eca0cec0db8eccff21f9a3530e91303ce
Ezra Schroeder <ezra.schroeder@gmail.com>
2023-02-15 16:27:12 -0600

    Time: 108 ms (5.05%), Space: 14.1 MB (25.65%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,20 +1,23 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        
         all_words = set(wordDict)
-        
-        @functools.lru_cache(None)
-        def word_break_recursive(s):
+        #@functools.lru_cache(None)
+        def word_break_recursive(s, cash):
             if len(s) == 0 or s in all_words:
                 return True 
             for word in all_words: 
                 for i in range(len(s)):
-                    if s[:i] in all_words and word_break_recursive(s[i:]):
-                        return True 
-            return False 
-            
-            
-            
-             
-        return word_break_recursive(s)
+                    if s[:i] in all_words:
+                        if s[i:] in cash: 
+                            if cash[s[i:]]:
+                                return True
+                        else:
+                            stop = word_break_recursive(s[i:], cash)
+                            cash[s[i:]] = stop 
+                            #print (self._stop)
+                            if stop:
+                                return True 
+            return False    
+        cach_e = {} 
+        return word_break_recursive(s, cach_e)
         

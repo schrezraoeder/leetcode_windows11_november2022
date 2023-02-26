cd369f0698bdc2015d0426e4403c2acf3faad189
Ezra Schroeder <ezra.schroeder@gmail.com>
2023-02-26 14:14:03 -0600

    Time: 33 ms (54.85%), Space: 13.9 MB (60.75%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,19 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        list_pattern = [item for item in pattern] 
+        print (list_pattern) 
+        list_string = s.split()
+        print (list_string)
+        if len(list_pattern) != len(list_string):
+            return False 
+        map_pattern_words = {}
+        for ix, item in enumerate(list_string):
+            if item in map_pattern_words: 
+                if list_pattern[ix] != map_pattern_words[item]:
+                    return False 
+            else: 
+                if list_pattern[ix] in list_pattern[:ix]:
+                    return False 
+                map_pattern_words[item] = list_pattern[ix] 
+        return True 
+                

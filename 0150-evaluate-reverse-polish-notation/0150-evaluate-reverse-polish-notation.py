45fd540fcb7c01d4a23812ae3f32cb7fca50f187
Ezra Schroeder <ezra.schroeder@gmail.com>
2023-02-26 13:44:03 -0600

    Time: 66 ms (81.86%), Space: 14.3 MB (93.87%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -4,12 +4,14 @@ class Solution:
         stack = [] # push non-operators; if we see an operator, pop the last two  
         op = {'+': add, '-': sub, '*': mul, '/': truediv}
         for token in tokens: 
-            if token in '+-/*':
-                b, a = stack.pop(), stack.pop()
-                result = int(op[token](a, b)) # round *toward* zero  
-                stack.append(result)  
-            else: 
-                stack.append(int(token)) 
+            # if token in '+-/*':
+                # b, a = stack.pop(), stack.pop()
+                # result = int(op[token](a, b)) # round *toward* zero  
+                # stack.append(result)  
+                
+            stack.append(int(op[token](stack.pop(-2), stack.pop())) if token in '+-/*' else int(token)) 
+            # else: 
+            #     stack.append(int(token)) 
         return stack[-1]
                 
         

e34ff1f4e4aee1e402a72413110908819bbc88d9
Ezra Schroeder <ezra.schroeder@gmail.com>
2023-03-08 15:39:56 -0600

    Time: 1074 ms (88.89%), Space: 26.6 MB (95.56%) - LeetHub

---
+++ 1673-find-the-most-competitive-subsequence/1673-find-the-most-competitive-subsequence.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def mostCompetitive(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        stack = []
+        m = len(nums) - k 
+        p = 0 
+        for num in nums: 
+            while stack and stack[-1] > num:
+                if p < m:
+                    stack.pop() 
+                    p += 1 
+                else:
+                    break 
+            stack.append(num) 
+        return stack[:k]
+            
+        

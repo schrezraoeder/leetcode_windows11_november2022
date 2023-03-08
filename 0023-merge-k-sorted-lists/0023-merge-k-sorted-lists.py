c430954d059d609e4012b408fb41a209b2191213
Ezra Schroeder <ezra.schroeder@gmail.com>
2023-03-08 13:30:07 -0600

    Time: 5652 ms (6.94%), Space: 19.7 MB (60.78%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -15,16 +15,11 @@ class Solution(object):
         while any(pointers):
             min_val = float('inf')
             for i in range(k):
-                if pointers[i]: # assuming node.next == None for end of list, thanks leetcode 
-                    # print ()
-                    # print ('pointers[i]')
-                    # print (pointers[i])
+                if pointers[i]: 
                     if pointers[i].val < min_val:
                         min_val = pointers[i].val 
                         increment_val_pointer = pointers[i] 
                         increment_pointer = i 
-                # else:
-                #     pointers[i] = False # change from None to False ? 
             if not answer:
                 answer = increment_val_pointer 
                 head = increment_val_pointer
@@ -33,7 +28,6 @@ class Solution(object):
                 answer.next = increment_val_pointer
                 answer = answer.next 
                 pointers[increment_pointer] = pointers[increment_pointer].next 
-        #answer.next = None 
         if not answer:
             return answer 
         return head 
